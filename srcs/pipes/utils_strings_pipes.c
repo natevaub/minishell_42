c58346c0b229f75c10bef9106be4f5a4ad02c20a
Christina Karl <christina_karl@outlook.de>
2023-07-26 16:51:11 +0200

    pipe

---
+++ srcs/pipes/utils_strings_pipes.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 17:38:36 by ckarl             #+#    #+#             */
+/*   Updated: 2023/06/22 12:28:28 by ckarl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+//join two strings and add '/' at the end
+char	*ft_join_path(char const *s1, char const *s2)
+{
+	char			*copy;
+	int				i;
+	int				j;
+	unsigned int	len1;
+	unsigned int	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	i = 0;
+	j = 0;
+	copy = (char *)malloc(sizeof(*copy) * (len1 + len2 + 1));
+	if (!copy)
+		return (NULL);
+	while (s1[i])
+	{
+		copy[i] = s1[i];
+		i++;
+	}
+	copy[i++] = '/';
+	while (s2[j])
+		copy[i++] = s2[j++];
+	copy[i] = '\0';
+	return (copy);
+}
